9f0e3cde883a2b4081009cb17bcb03bb9430f29a
bersentox <bersen.tox@gmail.com>
2026-03-12 17:17:11 +0300

    Extract configurator pricing constants to config file

---
+++ garage-configurator-new/config-prices.js
@@ -0,0 +1,38 @@
+window.CONFIG_PRICES = {
+  // Цена за квадратный метр по ширине гаража
+  RATE_PER_M2: {
+    6: 54000,
+    8: 57000
+  },
+
+  // Доплаты за планировку
+  LAYOUT_SURCHARGE: {
+    classic: 0,
+    storage: 135000,
+    utility: 240000
+  },
+
+  // Доплаты за тип крыши
+  ROOF_SURCHARGE: {
+    flat: 0,
+    gable: 130000,
+    shed: 85000
+  },
+
+  // Цены дополнительных элементов
+  ELEMENT_PRICE: {
+    shelves: 45000,
+    partition: 90000,
+    door: 32000,
+    window: 18000
+  },
+
+  // Цены дополнительных опций
+  OPTIONS_PRICE: {
+    gateAutomation: 110000,
+    interiorElectricity: 150000,
+    exteriorLighting: 70000,
+    ventilation: 60000,
+    gutters: 50000
+  }
+};
