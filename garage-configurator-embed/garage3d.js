3b528034e48e1963e966d121b96f244a31facf9c
bersentox <bersen.tox@gmail.com>
2026-03-23 23:17:56 +0300

    Update garage3d.js

--- garage-configurator-embed/garage3d.js
+++ garage-configurator-embed/garage3d.js
@@ -260,10 +260,6 @@ controls.maxPolarAngle = 1.42;
         centerModel(mountedModel);
         modelGroup.add(mountedModel);
         frameModel(mountedModel);
-          // сместить композицию влево
-          console.log("TARGET SHIFT TEST");
-          controls.target.x -= 10;
-          controls.update();
         const meshIndex = buildMeshIndex(mountedModel);
         Object.assign(garageParts, detectGarageParts(meshIndex));
         applyColors(activeColors);
